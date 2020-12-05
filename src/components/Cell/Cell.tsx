678c2c4ae4b828d8d408d9f9aba821b68678ee0f
Anton Mendelson <mendelson.inbox@gmail.com>
2020-12-05 11:14:06 +0200

    react memo

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { PropsWithChildren, useEffect, useState } from "react";
 import "./Cell.scss";
 
 interface Props {
   alive: boolean;
 }
 
-export const Cell: React.FC<Props> = React.memo(({ alive }) => {
-  return <div className={`Cell ${alive ? "alive" : "dead"}`}></div>;
-});
+export const Cell: React.FC<Props> = React.memo<Props>(
+  ({ alive }) => {
+    return <div className={`Cell ${alive ? "alive" : "dead"}`}></div>;
+  },
+  (prevProps, nextProps) => prevProps.alive === nextProps.alive
+);
